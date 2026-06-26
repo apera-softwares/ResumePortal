abe09d82a65d751f4b0f7d757fd5b0da3f82ac24
trushant <trushant@aperasoftwares.com>
2026-06-26 13:09:47 +0530

    Database Schema & Migration Alignment

--- Backend/prisma/migrations/20260615063908_status_change/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Candidate" ADD COLUMN     "jobId" INTEGER,
-ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'Applied';
-
--- AddForeignKey
-ALTER TABLE "Candidate" ADD CONSTRAINT "Candidate_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "Job"("id") ON DELETE CASCADE ON UPDATE CASCADE;
